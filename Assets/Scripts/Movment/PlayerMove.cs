7e3cea299a135f0411ab552ed3116e57856fad2b
Alp Eren Durak <alprendurak@gmail.com>
2023-11-17 13:59:02 +0300

    ZıplamaVeYerçekimi

--- Assets/Scripts/Movment/PlayerMove.cs
+++ Assets/Scripts/Movment/PlayerMove.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-
     [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 8f; // Zýplama kuvveti
     [SerializeField] private Transform orientation;
 
     private CharacterController controller;
+    private Vector3 playerVelocity;
 
     [HideInInspector]
     public bool canMove = true;
@@ -21,21 +22,58 @@ public class PlayerMove : MonoBehaviour
     void Update()
     {
         PlayerMovement();
+        ApplyGravity();
+
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            Jump();
+        }
     }
 
     void PlayerMovement()
     {
-        float horizontal=Input.GetAxisRaw("Horizontal");
-        float vertical =Input.GetAxisRaw("Vertical");
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
 
         Vector3 moveDirection = orientation.forward * vertical + orientation.right * horizontal;
-        controller.SimpleMove(moveDirection.normalized * moveSpeed);
 
-        if (horizontal != 0 || vertical != 0)
+        if (moveDirection != Vector3.zero)
         {
+            controller.Move(moveDirection.normalized * moveSpeed * Time.deltaTime);
             canMove = true;
         }
-        else canMove = false;
+        else
+        {
+            canMove = false;
+        }
+    }
 
+    void ApplyGravity()
+    {
+        if (controller.isGrounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        playerVelocity.y += Physics.gravity.y * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+
+    bool IsGrounded()
+    {
+        float rayDistance = controller.height * 0.5f + 0.1f;
+        Vector3 rayOrigin = transform.position + controller.center; 
+
+        if (Physics.Raycast(rayOrigin, Vector3.down, rayDistance))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    void Jump()
+    {
+        playerVelocity.y = Mathf.Sqrt(jumpForce * -2f * Physics.gravity.y);
     }
 }
