5e94589b3ce2ac2ddec4a869b001dce1e79a80da
Alp Eren Durak <alprendurak@gmail.com>
2023-11-18 20:05:54 +0300

    DenemeBölüm1

--- Assets/RotateManager.cs
+++ Assets/RotateManager.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class RotateManager : MonoBehaviour
 {
     RortateControl[] childRotates;
-    [SerializeField] private Light _light;
-
+    [SerializeField] private GameObject light1;
+    [SerializeField] private GameObject light2;
+    [SerializeField] private GameObject light3;
+    [SerializeField] private GameObject player;
+    bool endgame;
     void Start()
     {
         childRotates = GetComponentsInChildren<RortateControl>();
+        Invoke("LevelStart", 3);
     }
 
     void Update()
@@ -23,7 +27,23 @@ public class RotateManager : MonoBehaviour
                 childsFinished = false;
             }
         }
-
-        _light.enabled = childsFinished;
+        if (childsFinished&&endgame)
+        {
+            Invoke("endinggame", 3);
+        }
+    }
+    void LevelStart()
+    {
+        player.transform.position = new Vector3(-0.474f, -1.384f, 0.39f);
+        light1.SetActive(true);
+        light2.SetActive(true);
+        light3.SetActive(true);
+    }
+    void endinggame()
+    {
+        light1.SetActive(false);
+        light2.SetActive(false);
+        light3.SetActive(false);
+        player.transform.position = new Vector3(-19.27f, 6.167f, 66.907f);
     }
 }
